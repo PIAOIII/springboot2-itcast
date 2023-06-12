eed6f34e83fc334b88f1613a8b6a542f8d166c16
LAPTOP-U77VULRG\sun <sun.xy.piao@gmail.com>
2023-06-12 16:32:20 +0800

    temp

---
+++ springboot2/springboot_08_ssmp/src/main/java/com/xy/service/IBookService.java
@@ -0,0 +1,9 @@
+package com.xy.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xy.domain.Book;
+
+public interface IBookService extends IService<Book> {
+
+}
--- springboot2/springboot_08_ssmp/src/main/java/com/xy/service/impl/BookServiceImpl.java
+++ springboot2/springboot_08_ssmp/src/main/java/com/xy/service/impl/BookServiceImpl.java
@@ -1,50 +1,9 @@
 package com.xy.service.impl;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xy.dao.BookDao;
 import com.xy.domain.Book;
-import com.xy.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.xy.service.IBookService;
 
-import java.util.List;
-
-@Service
-public class BookServiceImpl implements BookService {
-
-    @Autowired
-    private BookDao bookDao;
-
-    @Override
-    public Boolean save(Book book) {
-        return bookDao.insert(book) > 0;
-    }
-
-    @Override
-    public Boolean update(Book book) {
-        return bookDao.updateById(book) > 0;
-    }
-
-    @Override
-    public Boolean delete(Integer id) {
-        return bookDao.deleteById(id) > 0;
-    }
-
-    @Override
-    public Book getById(Integer id) {
-        return bookDao.selectById(id);
-    }
-
-    @Override
-    public List<Book> getAll() {
-        return bookDao.selectList(null);
-    }
-
-    @Override
-    public IPage<Book> getPage(int currentPage, int pageSize) {
-        IPage page = new Page(currentPage, pageSize);
-        bookDao.selectPage(page, null);
-        return page;
-    }
+public class BookServiceImpl extends ServiceImpl<BookDao, Book> implements IBookService {
 }
